e21e0717166a0db2198ae3fd84a8d43dceafb441
virtualformacion <virtualformacion47@gmail.com>
2026-04-11 10:14:49 -0500

    Actualizar usuarios (delete) desde admin

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // ========== USUARIOS AUTORIZADOS ==========
 const USERS = [
     { username: "polo", password: "3107", expiresAt: new Date("2026-12-31") },
-    { username: "Garcias658392@outlook.com", password: "linda1234", expiresAt: new Date("2025-12-27") },
     { username: "Sorbete64736@outlook.com", password: "linda1234", expiresAt: new Date("2025-12-23") },
     { username: "santamarta64584@outlook.com", password: "linda1234", expiresAt: new Date("2025-12-20") },
     { username: "Teresa333torre@outlook.com", password: "linda1234", expiresAt: new Date("2025-12-20") }
